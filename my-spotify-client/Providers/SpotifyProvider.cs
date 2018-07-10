80eb34a1ccf3fbb230f1a646d7d7a000f59aa5d3
Victor Vilela <victor.vilela@outlook.com>
2018-07-10 15:08:32 -0500

    add SpotifyProvider class

---
+++ my-spotify-client/Providers/SpotifyProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace my_spotify_client.Providers
+{
+    public class SpotifyProvider
+    {
+    }
+}
