66265f6e9f27da9d278c5b84902714db3f94bcf4
Victor Vilela <victor.vilela@outlook.com>
2018-07-10 15:36:27 -0500

    moving methods to SpotifyProvider

--- my-spotify-client/Controllers/WebClientController.cs
+++ my-spotify-client/Controllers/WebClientController.cs
@@ -12,6 +12,7 @@ using my_spotify_client.Common;
 using my_spotify_client.Common.AppSettingsManager;
 using my_spotify_client.Common.SessionManager;
 using my_spotify_client.Models;
+using my_spotify_client.Providers;
 
 namespace my_spotify_client.Controllers
 {
@@ -25,11 +26,19 @@ namespace my_spotify_client.Controllers
             }
         }
 
-        private AppSettingsManager AppSettingsManager
+        public AppSettingsManager AppSettingsManager
         {
             get
             {
                 return AppSettingsManager.Instance();
+            }
+        }
+
+        private SpotifyProvider SpotifyProvider
+        {
+            get
+            {
+                return SpotifyProvider.Instance();
             }
         }
 
@@ -42,44 +51,16 @@ namespace my_spotify_client.Controllers
         public ActionResult RequestAuthorizationToSpotify()
         {
             SessionManager.State = DateTime.Now.Ticks.ToString();
-            //
-            var authorizeUrl = AppSettingsManager.SpotifyAccountsUrl + "/authorize?";
-            authorizeUrl += "client_id=" + AppSettingsManager.ClientId + "&";
-            authorizeUrl += "response_type=code&";
-            authorizeUrl += "redirect_uri=" + AppSettingsManager.RedirectUri + "&";
-            authorizeUrl += "scope=user-read-private user-read-email&";
-            authorizeUrl += "state=" + SessionManager.State;
-
-            return Redirect(authorizeUrl);
+            return Redirect(SpotifyProvider.GetSpotifyAuthorizationUrl(SessionManager.State));
         }
 
         public async Task<ActionResult> ProccessSpotifyResponse(string code = "", string error = "", string state = "")
         {
-            if (!string.IsNullOrWhiteSpace(error) || !state.Equals(SessionManager.State)) return View(new ProccessSpotifyResponseModel(true, error));
+            if (!string.IsNullOrWhiteSpace(error) || 
+                !state.Equals(SessionManager.State)) return View(new ProccessSpotifyResponseModel(true, error));
 
-            SessionManager.SpotifyToken = await GetSpotifyTokenAsync(code);
-
+            SessionManager.SpotifyToken = await SpotifyProvider.GetSpotifyTokenAsync(code);
             return View(new ProccessSpotifyResponseModel(false, string.Empty));
-        }
-
-        public async Task<SpotifyToken> GetSpotifyTokenAsync(string code)
-        {
-            var httpClient = new HttpClient
-            {
-                BaseAddress = new Uri(AppSettingsManager.SpotifyAccountsUrl)
-            };
-            var request = new HttpRequestMessage(HttpMethod.Post, "/api/token");
-            var keyValues = new List<KeyValuePair<string, string>>
-            {
-                new KeyValuePair<string, string>("grant_type", "authorization_code"),
-                new KeyValuePair<string, string>("code", code),
-                new KeyValuePair<string, string>("redirect_uri",AppSettingsManager.RedirectUri),
-                new KeyValuePair<string, string>("client_id",AppSettingsManager.ClientId),
-                new KeyValuePair<string, string>("client_secret",AppSettingsManager.ClientSecret)
-            };
-            request.Content = new FormUrlEncodedContent(keyValues);
-
-            return await httpClient.SendAsync(request).Result.Content.ReadAsAsync<SpotifyToken>();
         }
     }
 }
--- my-spotify-client/Providers/SpotifyProvider.cs
+++ my-spotify-client/Providers/SpotifyProvider.cs
@@ -1,11 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Mvc;
+using my_spotify_client.Common.AppSettingsManager;
+using my_spotify_client.Controllers;
+using my_spotify_client.Models;
 
 namespace my_spotify_client.Providers
 {
     public class SpotifyProvider
     {
+        private static SpotifyProvider _instance;
+
+        public static SpotifyProvider Instance()
+        {
+            if (_instance == null) _instance = new SpotifyProvider();
+
+            return _instance;
+        }
+
+        public AppSettingsManager AppSettingsManager
+        {
+            get
+            {
+                return AppSettingsManager.Instance();
+            }
+        }
+
+        public string GetSpotifyAuthorizationUrl(string state)
+        {
+            var authorizeUrl = AppSettingsManager.SpotifyAccountsUrl + "/authorize?";
+
+            authorizeUrl += "client_id=" + AppSettingsManager.ClientId + "&";
+            authorizeUrl += "response_type=code&";
+            authorizeUrl += "redirect_uri=" + AppSettingsManager.RedirectUri + "&";
+            authorizeUrl += "scope=user-read-private user-read-email&";
+            authorizeUrl += "state=" + state;
+
+            return authorizeUrl;
+        }
+
+        public async Task<SpotifyToken> GetSpotifyTokenAsync(string code)
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(AppSettingsManager.SpotifyAccountsUrl)
+            };
+            var request = new HttpRequestMessage(HttpMethod.Post, "/api/token");
+            var keyValues = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                new KeyValuePair<string, string>("code", code),
+                new KeyValuePair<string, string>("redirect_uri", AppSettingsManager.RedirectUri),
+                new KeyValuePair<string, string>("client_id", AppSettingsManager.ClientId),
+                new KeyValuePair<string, string>("client_secret", AppSettingsManager.ClientSecret)
+            };
+            request.Content = new FormUrlEncodedContent(keyValues);
+
+            return await httpClient.SendAsync(request).Result.Content.ReadAsAsync<SpotifyToken>();
+        }
     }
 }
